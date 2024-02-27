b989a92782a58f95e372519abb8ced020bba39f7
De <spark49942@gmail.com>
2024-02-27 13:10:29 +0300

    flaskdz-4-1

--- dz.py
+++ dz.py
@@ -1,18 +1,21 @@
 import requests, time, os
 from flask import Flask
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 from pathlib import *
+import threading
+import urllib.request
+import shutil
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/upload")
+# @app.get("./upload")
 def download(url, start_time):
+    response = urllib.request.urlopen(url)
     urls = url.split('/')
-    filename = "/upload/" + urls[-1]
-    # return FileResponse(path='url', filename=filename, media_type='multipart/form-data')
-    if os.path.exists(filename):
-        return FileResponse(path=filename, filename=filename, media_type='multipart/form-data')
+    filename = urls[-1]
+    with urllib.request.urlopen(url) as response, open(f"./upload/{filename}", 'wb') as file:
+        shutil.copyfileobj(response, file)
     print(f"Downloaded {filename} in {time.time()-start_time:.2f} seconds")
 
 def task1(urls: list[str]):
@@ -23,8 +26,8 @@ def task1(urls: list[str]):
 def main():
     Path(Path.cwd() / 'upload').mkdir(exist_ok=True)
     urls = [
-        'https://static.wikia.nocookie.net/cnc_gamepedia_en/images/1/12/Romanov2.png'
-        'https://static.wikia.nocookie.net/cnc_gamepedia_en/images/0/08/CNCR_Kane_Banner.png'
+        'https://static.wikia.nocookie.net/cnc_gamepedia_en/images/1/12/Romanov2.png', 
+        'https://static.wikia.nocookie.net/cnc_gamepedia_en/images/0/08/CNCR_Kane_Banner.png', 
         'https://static.wikia.nocookie.net/cnc_gamepedia_en/images/d/df/Tomahawk_Storm.jpg'
     ]
     task1(urls)
